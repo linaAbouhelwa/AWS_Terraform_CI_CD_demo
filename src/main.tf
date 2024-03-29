2648fd956d2a65a94cde92c650ba5359822a5f7b
linaAbouhelwa <149501575+linaAbouhelwa@users.noreply.github.com>
2024-03-29 23:42:03 +0000

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -10,10 +10,11 @@ terraform {
   }
 
   required_version = ">=0.13.0"
+  terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>3.0"
+      version = "~> 5.0"
     }
   }
 }
